9ec68f16d8ee98bd629bad881ffbdb1dba00c4db
Sriram Sami <frizensami@gmail.com>
2022-01-12 19:31:51 -0800

    Broken css config but absolute position for extension output

--- src/content/content.tsx
+++ src/content/content.tsx
@@ -10,17 +10,17 @@ function Welcome(props: any) {
 // Create CSS Grid/Flex at the root
 const wrapperDiv = document.createElement("div");
 wrapperDiv.id = "content-script-wrapper"
-wrapperDiv.className = 'grid grid-cols-4 gap-2'
+wrapperDiv.className = 'static'
 
-// Create the wrapper for the existing content
-const mainContentWrapperDiv = document.createElement("div");
-mainContentWrapperDiv.id = "main-content-wrapper"
-mainContentWrapperDiv.className = "col-span-3"
-wrapperDiv.appendChild(mainContentWrapperDiv);
+// // Create the wrapper for the existing content
+// const mainContentWrapperDiv = document.createElement("div");
+// mainContentWrapperDiv.id = "main-content-wrapper"
+// mainContentWrapperDiv.className = "col-span-3"
+// wrapperDiv.appendChild(mainContentWrapperDiv);
 
 // Move the body's children into this wrapper
 while (document.body.firstChild) {
-    mainContentWrapperDiv.appendChild(document.body.firstChild);
+    wrapperDiv.appendChild(document.body.firstChild);
 }
 
 // Make the wrapper div the new body
@@ -29,6 +29,6 @@ document.body.appendChild(wrapperDiv);
 // Now append the content as a 1/4th grid col
 const contentDiv = document.createElement("div");
 contentDiv.id = "content-script-div";
-contentDiv.className = "col-span-1 border-green";
+contentDiv.className = "fixed bottom-0 right-0";
 wrapperDiv.appendChild(contentDiv)
 ReactDOM.render(<Welcome/>, contentDiv);
