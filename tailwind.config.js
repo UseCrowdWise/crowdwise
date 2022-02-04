a62ffd7f446fad04bc43dd48cf255edc23a0035e
Yew Siang <tangys108@gmail.com>
2022-02-05 00:19:39 +0800

    Update packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
   corePlugins: {
-    preflight: false,
+    preflight: true,
   },
 };
