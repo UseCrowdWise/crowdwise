824a7ccc1db71cd3c5eb06c1c4b488b862b19a3f
Yew Siang <tangys108@gmail.com>
2022-02-05 00:19:16 +0800

    Add keyboard shortcuts to trigger and close sidebar

--- src/content/ContentButton.tsx
+++ src/content/ContentButton.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./content.css";
+import { useHotkeys } from "react-hotkeys-hook";
 import { ProviderResults } from "../providers/providers";
 import { AcademicCapIcon } from "@heroicons/react/solid";
 
@@ -9,25 +10,40 @@ interface Props {
 }
 
 export default function ContentButton(props: Props) {
-  const { providerData } = props;
+  const { providerData, onClicked } = props;
   const numResults =
     providerData.hackerNews.length + providerData.reddit.length;
   const hasResults = numResults > 0;
+
+  // Trigger button on certain hotkeys
+  useHotkeys("cmd+j", () => onClicked());
+
   return (
-    <button
-      className="fixed bottom-0 right-0 z-[2000000000] rounded-full text-lg w-12 h-12 mr-6 mb-5 bg-blue-700 text-white hover:text-gray-400 focus:outline-none focus:text-gray-500 transition duration-150 ease-in-out"
-      onClick={props.onClicked}
-    >
-      <div className="relative">
-        <AcademicCapIcon className="w-8 h-8 stroke-0 mx-auto" />
-        {hasResults && (
-          <span className="absolute -right-2 -top-4">
-            <div className="inline-flex items-center px-1.5 py-0.5 rounded-md text-xs font-semibold bg-red-600 text-white">
-              {numResults}
-            </div>
-          </span>
-        )}
+    <div className="fixed bottom-0 right-0 z-[2000000000]">
+      <button
+        data-tooltip-target="tooltip-default"
+        className="rounded-full text-lg w-12 h-12 mr-6 mb-5 bg-blue-700 text-white hover:text-gray-400 focus:outline-none focus:text-gray-500 transition duration-150 ease-in-out"
+        onClick={onClicked}
+      >
+        <div className="relative">
+          <AcademicCapIcon className="w-8 h-8 stroke-0 mx-auto" />
+          {hasResults && (
+            <span className="absolute -right-2 -top-4">
+              <div className="inline-flex items-center px-1.5 py-0.5 rounded-md text-xs font-semibold bg-red-600 text-white">
+                {numResults}
+              </div>
+            </span>
+          )}
+        </div>
+      </button>
+      <div
+        id="tooltip-default"
+        role="tooltip"
+        className="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip dark:bg-gray-700"
+      >
+        Cmd + J
+        <div className="tooltip-arrow" data-popper-arrow />
       </div>
-    </button>
+    </div>
   );
 }
