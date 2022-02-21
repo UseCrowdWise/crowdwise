b7b0f81f1c20c0804cb45dbe73c4a07f4502d872
Yew Siang <tangys108@gmail.com>
2022-02-21 21:58:10 +0800

    Convert to functional component and allow toggle of sidebar visibility

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -2,7 +2,7 @@ import { printLine } from "./modules/print";
 import ReactDOM from "react-dom";
 import { APP_NAME_SHORT } from "../../shared/constants";
 import cx from "classnames";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useHotkeys } from "react-hotkeys-hook";
 // import './helpers/SidebarHelper';
 
@@ -12,7 +12,6 @@ console.log("Must reload extension for modifications to take effect.");
 printLine("Using the 'printLine' function from the Print Module");
 
 let sidebarRoot = document.createElement("div");
-// sidebarRoot.innerText = "HELLO WORLD!";
 sidebarRoot.style["height"] = "100vh";
 sidebarRoot.style["top"] = 0;
 sidebarRoot.style["right"] = 0;
@@ -24,54 +23,60 @@ sidebarRoot.setAttribute("id", "vt-sidebar-root");
 
 // First react component that is rendered onto vt-sidebar-root
 // This had to be a class so we can mount and unmount chrome listeners
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      shouldShowSideBar: true,
-    };
-    this.handleMessage.bind(this);
-  }
-  componentDidMount() {
-    // Add listener when component mounts
-    chrome.runtime.onMessage.addListener(this.handleMessage);
-  }
-  componentWillUnmount() {
-    // Remove listener when this component unmounts
-    chrome.runtime.onMessage.removeListener(this.handleMessage);
-  }
+const App = () => {
+  const [shouldShowSideBar, setShouldShowSideBar] = useState(true);
 
   // Close the side bar based on incoming message from further down in the component (close arrow)
-  handleMessage = (request, sender, sendResponse) => {
+  const handleMessage = (request, sender, sendResponse) => {
     console.log(
       sender.tab
         ? "from a content script:" + sender.tab.url
         : "from the extension"
     );
     if (request.closeSideBar === true) {
-      this.setState({ shouldShowSideBar: false });
+      setShouldShowSideBar((show) => !show);
     }
   };
 
-  render() {
-    return (
-      <div>
-        <iframe
-          title="sidebar-iframe"
+  useEffect(() => {
+    // Add listener when component mounts
+    chrome.runtime.onMessage.addListener(handleMessage);
+
+    // Remove listener when this component unmounts
+    return () => chrome.runtime.onMessage.removeListener(handleMessage);
+  }, []);
+
+  return (
+    <div>
+      <iframe
+        title="sidebar-iframe"
+        style={{
+          width: shouldShowSideBar ? "auto" : "0",
+          height: "100vh",
+          border: "none",
+          borderSizing: "border-box",
+          opacity: 0.95,
+        }}
+        src={chrome.runtime.getURL("sidebar.html")}
+        // ref={(frame) => (this.frame = frame)}
+        onLoad={() => console.log("iFrame loaded")}
+      />
+      {!shouldShowSideBar && (
+        <div
           style={{
-            width: this.state.shouldShowSideBar ? "auto" : "0",
-            height: "100vh",
-            border: "none",
-            borderSizing: "border-box",
-            opacity: 0.95,
+            position: "fixed",
+            bottom: "10px",
+            right: "10px",
+            borderRadius: "100%",
+            width: "100px",
+            height: "100px",
+            backgroundColor: "red",
           }}
-          src={chrome.runtime.getURL("sidebar.html")}
-          // ref={(frame) => (this.frame = frame)}
-          onLoad={() => console.log("iFrame loaded")}
+          onClick={() => setShouldShowSideBar((show) => !show)}
         />
-      </div>
-    );
-  }
-}
+      )}
+    </div>
+  );
+};
 
 ReactDOM.render(<App />, sidebarRoot);
