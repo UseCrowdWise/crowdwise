cb01f70ae730f9f9ad285e67f5533ba24dbf0464
Yew Siang <tangys108@gmail.com>
2022-02-23 22:54:18 +0800

    Able to select placement of content button in settings

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,3 +1,5 @@
+import { CONTENT_BUTTON_PLACEMENT_OPTIONS } from "./options";
+
 export const APP_NAME_SHORT = "vt";
 export const APP_NAME_FULL = "Vertical Tabs";
 export const SIDEBAR_CONTAINER_ID = `${APP_NAME_SHORT}-sidebar-container`;
@@ -6,6 +8,9 @@ export const SETTINGS_DEBOUNCE_TIME = 400;
 export const DEFAULT_SIDEBAR_WIDTH = 24; // rem
 export const DEFAULT_SIDEBAR_OPACITY = 95; // Divided by 100
 export const DEFAULT_HIDE_CONTENT_BUTTON = false;
+export const DEFAULT_CONTENT_BUTTON_PLACEMENT =
+  CONTENT_BUTTON_PLACEMENT_OPTIONS[3];
+export const DEFAULT_CONTENT_BUTTON_PLACEMENT_OFFSET = "16px";
 
 // Hotkeys. See reference for special shortcuts.
 // https://github.com/JohannesKlauss/react-hotkeys-hook
@@ -17,6 +22,7 @@ export const KEY_HOTKEYS_TOGGLE_SIDEBAR = "hotkeys-toggle-sidebar";
 export const KEY_SIDEBAR_WIDTH = "sidebar-width";
 export const KEY_SIDEBAR_OPACITY = "sidebar-opacity";
 export const KEY_HIDE_CONTENT_BUTTON = "hide-content-button";
+export const KEY_CONTENT_BUTTON_PLACEMENT = "content-button-placement";
 
 // Cache settings
 export const CACHE_URL_DURATION_SEC = 60;
