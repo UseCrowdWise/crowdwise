b35a92f9f3e3902539e73861d6c4182fe20aca54
Yew Siang <tangys108@gmail.com>
2022-02-25 17:07:51 +0800

    Spinner animation on extension button when fetching results

--- src/pages/Sidebar/Sidebar.tsx
+++ src/pages/Sidebar/Sidebar.tsx
@@ -161,7 +161,7 @@ const Sidebar = () => {
         <div className="fixed top-0 left-0 right-0 bottom-0 z-50 flex h-screen w-full flex-col items-center justify-center overflow-hidden bg-gray-700 opacity-75">
           <div className="loader mb-4 h-12 w-12 rounded-full border-4 border-t-4 ease-linear" />
           <h2 className="text-center text-xl font-semibold text-white">
-            Loading backlinks...
+            Loading discussions...
           </h2>
         </div>
       )}
