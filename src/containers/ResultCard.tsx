6f86155a45c7ff5bb4ca9e51ddff4f6b9a481238
Yew Siang <tangys108@gmail.com>
2022-02-26 13:59:32 +0800

    Color names of article authors

--- src/containers/ResultCard.tsx
+++ src/containers/ResultCard.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { ResultItem } from "../providers/providers";
 import { ChatIcon, ThumbUpIcon } from "@heroicons/react/solid";
-import { KEY_FONT_SIZES } from "../shared/constants";
+import {
+  COLOR_IF_OUTSIDE_HASH,
+  KEY_FONT_SIZES,
+  KEY_SHOULD_COLOR_FOR_SUBMITTED_BY,
+} from "../shared/constants";
 import { useSettingsStore } from "../shared/settings";
+import { hashStringToColor } from "../utils/color";
 
 interface Props {
   result: ResultItem;
@@ -15,6 +20,9 @@ const ResultCard = (props: Props) => {
     useSettingsStore();
 
   const fontSizes = settings[KEY_FONT_SIZES];
+  const colorForSubmittedBy = settings[KEY_SHOULD_COLOR_FOR_SUBMITTED_BY]
+    ? hashStringToColor(result.submittedBy)
+    : COLOR_IF_OUTSIDE_HASH;
 
   return (
     <div
@@ -24,7 +32,7 @@ const ResultCard = (props: Props) => {
       {result.subSourceName !== "" && (
         <div className="flex flex-row space-x-1">
           <div
-            className={`${fontSizes.subtext} font-medium text-slate-500 hover:underline`}
+            className={`${fontSizes.subText} font-medium text-slate-500 hover:underline`}
           >
             <a
               href={result.subSourceLink}
@@ -48,7 +56,7 @@ const ResultCard = (props: Props) => {
           {result.submittedTitle}
         </a>
       </div>
-      <div className={`${fontSizes.subText} flex flex-row space-x-3`}>
+      <div className={`${fontSizes.subText} flex flex-row flex-wrap space-x-3`}>
         <div className="flex flex-row items-center space-x-1">
           <strong className="text-slate-500">{result.submittedUpvotes}</strong>
           <ThumbUpIcon className="h-3 w-3 text-slate-300" />
@@ -59,7 +67,7 @@ const ResultCard = (props: Props) => {
         </div>
         <div className="text-slate-600">{result.submittedDate}</div>
         <div className="grow" />
-        <div className="text-[10px] text-slate-600 hover:underline">
+        <div className={`text-[11px] ${colorForSubmittedBy} hover:underline`}>
           <a
             href={result.submittedByLink}
             target="_blank"
