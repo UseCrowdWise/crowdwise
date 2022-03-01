1a92d913ee2357d2287b25ae0c34b15724ff4c71
Sriram Sami <frizensami@gmail.com>
2022-03-01 23:06:41 +0800

    Integrate new result type with Sidebar.tsx but leave errors there

--- src/providers/providers.ts
+++ src/providers/providers.ts
@@ -9,9 +9,23 @@ import { RedditResultProvider } from "./reddit";
 // All providers must implement these two functions for search
 export interface ResultProvider {
   getProviderName(): string;
-  getExactUrlResults(url: string): Promise<ResultItem[]>;
-  getSiteUrlResults(url: string): Promise<ResultItem[]>;
-  getTitleResults(title: string): Promise<ResultItem[]>;
+  getExactUrlResults(url: string): Promise<SingleProviderResults>;
+  getSiteUrlResults(url: string): Promise<SingleProviderResults>;
+  getTitleResults(title: string): Promise<SingleProviderResults>;
+}
+
+// To indicate inside the result structure, so we know where in the UI to place it
+export enum ProviderQueryType {
+  EXACT_URL = "ExactUrl",
+  SITE_URL = "SiteUrl",
+  TITLE = "Title",
+}
+
+// A list of results from a provider call should have this form
+export interface SingleProviderResults {
+  providerName: string;
+  queryType: ProviderQueryType;
+  results: ResultItem[];
 }
 
 // All providers must return a list of resultitems
@@ -38,20 +52,23 @@ export enum ProviderResultType {
   Ok = "OK",
   Blacklisted = "BLACKLISTED",
 }
+// export const Providers = {[PROVIDER_HN_NAME]: {}}
+
+// NOTE: if we add more providers, remember to update code that adds the length of these together
+//  to get total result counts.
+export interface AllProviderResults {
+  resultType: ProviderResultType;
+  providerResults: SingleProviderResults[];
+  // hackerNews: ResultItem[];
+  // reddit: ResultItem[];
+}
 
 // Initialize all providers
 const reddit = new RedditResultProvider();
 const hackernews = new HnResultProvider();
 
-// export const Providers = {[PROVIDER_HN_NAME]: {}}
-
-// NOTE: if we add more providers, remember to update code that adds the length of these together
-//  to get total result counts.
-export interface ProviderResults {
-  resultType: ProviderResultType;
-  hackerNews: ResultItem[];
-  reddit: ResultItem[];
-}
+// Build a data structure of all providers so we can do things programmatically
+const providers: ResultProvider[] = [reddit, hackernews];
 
 /**
  * Main entry point for content script to get provider data.
@@ -60,7 +77,7 @@ export interface ProviderResults {
 export async function fetchDataFromProviders(
   rawUrl: string,
   documentTitle: string
-): Promise<ProviderResults> {
+): Promise<AllProviderResults> {
   log.debug("Starting to fetch provider data.");
 
   // Get just the site from the URL for a wider search.
@@ -93,8 +110,7 @@ export async function fetchDataFromProviders(
     log.warn(`URL ${cleanedUrl} or ${siteUrl} is blacklisted!`);
     return {
       resultType: ProviderResultType.Blacklisted,
-      hackerNews: [],
-      reddit: [],
+      providerResults: [],
     };
   }
 
@@ -104,20 +120,52 @@ export async function fetchDataFromProviders(
   // const providers = [hackernews, reddit]
   // const [hnExactResults, hnSiteResults, hnTitleResults, redditExactResults, redditSiteResults, reddit]= hackernews.getUrlResults(cleanedUrl);
 
-  // Call each provider in turn
-  const hnResults = await hackernews.getExactUrlResults(cleanedUrl);
-  log.debug("HN results:");
-  log.debug(hnResults);
+  // Gather all the promises from all the providers
+  // TODO if this order of calls changes, have to update the mapping function below
+  const providerPromises: Promise<SingleProviderResults>[] = providers
+    .map((provider) => [
+      provider.getExactUrlResults(cleanedUrl),
+      provider.getSiteUrlResults(siteUrl),
+      provider.getTitleResults(documentTitle),
+    ])
+    .flat();
 
-  const redditResults = await reddit.getExactUrlResults(cleanedUrl);
-  log.debug("Reddit results:");
-  log.debug(redditResults);
+  // Run them all concurrently and don't die early if one of the calls fails.
+  const allProviderPromiseResults: PromiseSettledResult<SingleProviderResults>[] =
+    await Promise.allSettled(providerPromises);
+
+  // We could have this be a constant, but this way we don't have to change it as we add more calls per provider.
+  const resultsPerProvider = providerPromises.length / providers.length;
+  // Process the results (depends on whether promises were accepted or rejected
+  const allResults: SingleProviderResults[] = allProviderPromiseResults
+    .map(
+      (providerPromiseResult: PromiseSettledResult<SingleProviderResults>) => {
+        if (providerPromiseResult.status === "rejected") {
+          log.error(
+            `Provider result rejected, reason: ${providerPromiseResult.reason}`
+          );
+          return undefined;
+        } else {
+          const result: SingleProviderResults = providerPromiseResult.value;
+          return result;
+        }
+      }
+    )
+    .filter((result) => result !== undefined) as SingleProviderResults[];
+
+  // Call each provider in turn
+  // const hnResults = await hackernews.getExactUrlResults(cleanedUrl);
+  // log.debug("HN results:");
+  // log.debug(hnResults);
+
+  // const redditResults = await reddit.getExactUrlResults(cleanedUrl);
+  // log.debug("Reddit results:");
+  // log.debug(redditResults);
 
   // TODO MUST DEDUPLICATE!
 
   return {
     resultType: ProviderResultType.Ok,
-    hackerNews: hnResults,
-    reddit: redditResults,
+    providerResults: allResults,
   };
 }
